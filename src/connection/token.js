ac51b4f1d364012bde82f2bb7c27b8d6d3925105
Ivan Shalaginov <shalaginov.dev@gmail.com>
2026-04-11 01:08:20 +0300

    add proxy-agent / token.js

--- src/connection/token.js
+++ src/connection/token.js
@@ -1,4 +1,10 @@
 import { Telegraf } from 'telegraf'
+import { ProxyAgent } from 'proxy-agent'
 import 'dotenv/config'
 
-export const bot = new Telegraf(process.env.BOT_TOKEN)
+const proxyUrl = process.env.ALL_PROXY || process.env.HTTPS_PROXY || process.env.HTTP_PROXY
+const agent = proxyUrl ? new ProxyAgent(proxyUrl) : undefined
+
+export const bot = new Telegraf(process.env.BOT_TOKEN, {
+	telegram: { agent },
+})
